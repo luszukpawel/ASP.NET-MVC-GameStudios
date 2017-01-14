b5f6fb50124a06199bb63b16007db2dc13a7d336
pluszak <luszukpawel@gmail.com>
2017-01-14 12:58:45 +0100

    basic bootstrap-cyborg implementation

---
+++ GamesStudios/GamesStudios/Controllers/GamesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GamesStudios.Models;
+
+namespace GamesStudios.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Random()
+        {
+            var game = new Game() {Name = "The Wish"};
+
+            return View(game);
+        }
+    }
+}
