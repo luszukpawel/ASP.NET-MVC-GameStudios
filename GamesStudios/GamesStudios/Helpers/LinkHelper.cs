8670891d83d922a9c4a931cc42141837c793fd28
pluszak <luszukpawel@gmail.com>
2017-01-19 22:33:48 +0100

    estetic changes + add helper

---
+++ GamesStudios/GamesStudios/Helpers/LinkHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+
+namespace GamesStudios.Helpers
+{
+    public static class LinkHelper
+    {
+        public static string CreateLinkButtton(string downloadLink, string buttonText)
+        { 
+           // return String.Format("< p >< a href =" + downloadLink + "class=\"btn btn-primary btn-lg\"> " + buttonText + "&raquo;</a></p>");
+         //   return String.Format("< p >< a href =" +"\""+ downloadLink + "\""+ " class=\"btn btn-primary btn-lg\"> " + buttonText + "&raquo;</a></p>");
+            return "<p><a href =" + "\"" + downloadLink + "\"" + " class=\"btn btn-primary btn-lg\"> " + buttonText + " &raquo;</a></p>";
+        }
+
+
+        public static string Truncate(this HtmlHelper helper, string input, int length)
+        {
+            if (input.Length <= length)
+            {
+                return input;
+            }
+            else
+            {
+                return input.Substring(0, length) + "...";
+            }
+        }
+    }
+}
